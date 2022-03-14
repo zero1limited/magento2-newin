c5bd90c1779c30cc45c52796049d0f47653985db
callum-zero1 <callum.breeze@zero1.co.uk>
2022-03-14 13:10:52 +0000

    Add filters for product status and stock status

--- Model/Cron/SetDates.php
+++ Model/Cron/SetDates.php
@@ -2,6 +2,9 @@
 namespace Zero1\NewIn\Model\Cron;
 
 use \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use \Magento\CatalogInventory\Model\Stock\StockItemRepository;
+use \Magento\Catalog\Model\Product\Attribute\Source\Status;
+use Magento\Framework\Exception\NoSuchEntityException;
 
 class SetDates
 {
@@ -11,12 +14,20 @@ class SetDates
     protected $productCollectionFactory;
 
     /**
+     * @var \Magento\CatalogInventory\Model\Stock\StockItemRepository
+     */
+    protected $stockItemRepository;
+
+    /**
      * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
+     * @param \Magento\CatalogInventory\Model\Stock\StockItemRepository $stockItemRepository
      */
     public function __construct(
-        CollectionFactory $productCollectionFactory
+        CollectionFactory $productCollectionFactory,
+        StockItemRepository $stockItemRepository
     ){
         $this->productCollectionFactory = $productCollectionFactory;
+        $this->stockItemRepository = $stockItemRepository;
     }
 
     public function execute()
@@ -25,6 +36,20 @@ class SetDates
         $productCollection->addAttributeToSelect('*');
 
         foreach($productCollection as $product) {
+            if($product->getStatus() != Status::STATUS_ENABLED) {
+                continue;
+            }
+
+            try {
+                $stockItem = $this->stockItemRepository->get($product->getId());
+            } catch(NoSuchEntityException $e) {
+                continue;
+            }
+
+            if(!$stockItem->getIsInStock()) {
+                continue;
+            }
+
             if($product->getNewsToDate() === null) {
                 $date = new \DateTime();
                 $interval = new \DateInterval('P30D');
